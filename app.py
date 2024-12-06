029c7f5b7a7af117fa22f9e7e578ef3c9c122525
MariyahWorkspace <dhansemariya@gmail.com>
2024-12-06 12:45:15 +0530

    Dynamic database data get, post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from database import load_courses
+from database import load_courses, load_course, add_application_to_db
 
 app = Flask(__name__)
 
@@ -13,9 +13,31 @@ def course_list():
   courses = load_courses()
   return jsonify(courses)
 
+@app.route("/courses/<id>")
+def show_course(id):
+  course = load_course(id)
+  return render_template("course-page.html", course = course)
+
+@app.route("/courses/<id>/apply", methods=["GET", "POST"])
+def apply_course(id):
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        motivation = request.form.get("motivation")
+    data = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "motivation": motivation
+        }
+    course = load_course(id)
+    add_application_to_db(id, data)
+    return render_template("submit.html", application=data, course=course)
+
 @app.route("/about", methods=["GET", "POST"])
 def about():
-  return render_template(("about.html"))
+  return render_template("about.html")
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
--- database.py
+++ database.py
@@ -18,3 +18,31 @@ def load_courses():
     courses = [dict(zip(columns, row)) for row in result]
     conn.close()
     return courses
+
+def load_course(id):
+    conn = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+    mycursor = conn.cursor(dictionary=True)
+    mycursor.execute("SELECT * FROM courses WHERE id = %s", (id,))
+    row = mycursor.fetchone()
+    conn.close()
+    if row is None:
+        return {"error": "Course record does not exist!"}
+    else:
+         return row
+    
+def add_application_to_db(course_id, application):
+    conn = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+    mycursor = conn.cursor()
+    sql_query = "INSERT INTO applications (course_id, name, email, phone, motivation) VALUES (%s, %s, %s, %s, %s)"
+    
+    data = (
+        course_id,
+        application.get('name'),
+        application.get('email'),
+        application.get('phone'),
+        application.get('motivation')
+    )
+    mycursor.execute(sql_query, data)
+    conn.commit()
+    conn.close()
+
